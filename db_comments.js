978094e9f8a2b18774792e2e388f3aaca309da8f
Tanmay sharma <tanmays95@gmail.com>
2017-03-06 18:14:45 +0530

    Revert "first commit"
    This reverts commit 55bbbdc788ab645f74e56996f92457abcc63a1a8.

--- db_comments.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-
-var commentsSchema = mongoose.Schema({
-    text: String,
-    author: String
-});
-module.exports = mongoose.model("Comment", commentsSchema);
